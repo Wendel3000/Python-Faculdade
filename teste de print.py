41c45cf2b649381e265694b2d838bbb4e6a71d65
Wendel3000 <116903239+Wendel3000@users.noreply.github.com>
2023-03-08 12:03:58 -0300

    Add files via upload

---
+++ teste de print.py
@@ -0,0 +1,21 @@
+# teste usando %s para substituir o nome >>>>>>>>>>>>>>>>>>>>>>
+
+nome = "Wendel"
+
+print ("Olá, Boa tarde %s , tenha um Bom dia" %(nome))
+
+# teste 2 normal >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+ 
+nome2 = "Wendel" 
+
+print ("Olá, Boa tarde " + nome2 + " , Tenha um Bom Dia!!!")
+
+# teste 3 usando.format >>>>>>>>>>>>>>>>>>>>>>>
+
+nome3 = "Wendel"
+print ("Olá, Boa tarde {} , tenha um Bom dia".format(nome3))
+
+# teste 4 usando strings formatadas >>>>>>>>>>>>>>>>>>>>>>>>>
+
+nome4 = "Wendel"
+print (f"Olá, Boa tarde {nome4} , tenha um Bom dia")
